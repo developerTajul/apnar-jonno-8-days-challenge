0a18c897a137f8bbfe1f4b156e38fff2d36460a7
dishowrov <dinislamshowrov2001@gmail.com>
2025-02-27 12:42:07 +0600

    logo issue

--- js/custom.js
+++ js/custom.js
@@ -403,16 +403,10 @@
     $(window).on('scroll', function () {
       if ($(window).scrollTop() > 50) {
         $('.fixed-menu').addClass('scrolled');
+        $('.standard-logo').hide();
+        $('.sticky-logo').show();
       } else {
         $('.fixed-menu').removeClass('scrolled');
-      }
-    });
-
-    $(window).on('scroll', function () {
-      if ($(window).scrollTop() < 50) {
-        $('.standard-logo').show();
-        $('.sticky-logo').hide();
-      } else {
         $('.standard-logo').show();
         $('.sticky-logo').hide();
       }
